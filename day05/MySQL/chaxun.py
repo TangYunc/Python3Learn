cb006eb98af72bc1b56f5cd1e32ac32962e314b5
tangyunchuan <tangyunchuan@yuanxin-inc.com>
2021-04-15 18:06:45 +0800

    Python3与MySQL

---
+++ day05/MySQL/chaxun.py
@@ -0,0 +1,30 @@
+import pymysql
+
+#1.建立数据库连接
+db = pymysql.connect("localhost", "root", "123456", "db2", charset="utf8")
+
+
+sql_select = "select * from city;"
+#2创建游标对象
+cursor = db.cursor(sql_select)
+
+data = cursor.fetchone()
+print("fetchone的结果为",data)
+
+data2 = cursor.fetchmany(2)
+print("fetchmany(2)的结果为",data2)
+
+data3 = cursor.fetchall()
+print("fetchall()的结果为",data3)
+
+#3.使用游标对象的方法操作数据库
+cursor.execute("insert into sheng values(1,20000,\'四川省');")
+
+
+
+#4.提交commit
+db.commit()
+#5.关闭游标对象
+cursor.close()
+#6.关闭数据库连接
+db.close()
